7f6d57074d6b0a6492b8c7f970b66c29f6e872ce
denilsonss <jhondoe@exemple.com>
2021-11-09 18:40:29 -0300

    Var e conversões

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// boolean 
+
+// conversao implicita
+    const numero = 456;
+    const numeroString = '456';
+
+    //console.log(numero === numeroString); => false  
+    //console.log(numero == numeroString); => true
+    //console.log(numero + numeroString); => vira uma concatenação
+
+    
+
+// conversao explicita
+//Number(); => quero passar algo para numero
+//String(); => quero passar algo para texto
+// console.log(numero + Number(numeroString));
---
+++ truthy-falsy.js
@@ -0,0 +1,29 @@
+//boolean 
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy and falsy
+
+// o => false
+// 1 => true
+
+//console.log(0 == false); isso é true
+//console.log('' == false); isso é true
+//console.log(1 == true); isso é true;
+
+//null => vazio ou nada é um objeto
+let minhaVar;
+let varNull = null
+
+//console.log(minhaVar) => undefined
+//console.log(varNull) => null
+
+let numero = 3;
+let text = 'Dida';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+//undefined
+
+
+
