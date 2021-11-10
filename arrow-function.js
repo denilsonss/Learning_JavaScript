dd4d003954ff6726e399ceecd10c201f22f03ce5
Denilson Sousa Soares <81116132+denilsonss@users.noreply.github.com>
2021-11-10 11:13:11 -0300

    Update arrow-function.js

--- arrow-function.js
+++ arrow-function.js
@@ -4,8 +4,8 @@ const apresentarArrow = nome => `meu nome é ${nome}`
 console.log('Dida')
 // arrow function com mais de uma linha de intrunção
 
-const somaNumerosPequenos = (num1 = false, num2 = 1) => {
-    if(num1 === false) {
+const somaNumerosPequenos = (num1, num2) => {
+    if(num1 > 10 || num2 > 10) {
         return "não soma numero maior que 10";
     } else {
         return num1 + num2;}
